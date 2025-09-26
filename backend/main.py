30f0c4a2a10de0b81920b2471373e8f5f36f677f
Zhang Qillin <135825398+zhangqillin231@users.noreply.github.com>
2025-09-26 14:17:51 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -174,6 +174,7 @@ def health_check():
     return {"status": "ok"}
 
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
     uvicorn.run('backend.main:app', host='0.0.0.0', port=port, reload=False)
